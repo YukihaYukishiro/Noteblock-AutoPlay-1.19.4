ebfab97de769e8e388674bd2d204b64459b3753a
YukihaYukishiro <appletea0514@gmail.com>
2024-04-22 10:21:15 +0900

    client instance

--- src/main/java/com/github/yukihayukishiro/auto_nbt/AutoNbsPlayer.java
+++ src/main/java/com/github/yukihayukishiro/auto_nbt/AutoNbsPlayer.java
@@ -3,10 +3,12 @@ package com.github.yukihayukishiro.auto_nbt;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.client.MinecraftClient;
 
 public class AutoNbsPlayer implements ModInitializer {
+	public static final MinecraftClient MC = MinecraftClient.getInstance();
 	public static final String MOD_ID = "auto_nbs_player";
-    public static final Logger LOGGER = LoggerFactory.getLogger("MOD_ID");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
