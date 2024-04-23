b3b7ec831afcd6d4a323e236797f5cfef06ac052
YukihaYukishiro <appletea0514@gmail.com>
2024-04-23 23:47:15 +0900

    generate directory

--- src/main/java/com/github/yukihayukishiro/auto_nbt/AutoNbsPlayer.java
+++ src/main/java/com/github/yukihayukishiro/auto_nbt/AutoNbsPlayer.java
@@ -1,19 +1,58 @@
 package com.github.yukihayukishiro.auto_nbt;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AutoNbsPlayer implements ModInitializer {
+	
+	public static final MinecraftClient MC = MinecraftClient.getInstance();
+
 	public static final String MOD_ID = "auto_nbs_player";
     public static final Logger LOGGER = LoggerFactory.getLogger("MOD_ID");
+	public static final Path SONG_DIR = Path.of("nbsplayer");
+	public static final Path SONG_FILE = Path.of("nbsplayer", "nbs");
+	public static final Path PLAYLIST_PATH = Path.of("nbsplayer", "playlist");
 
+	
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
+
+		if (!Files.exists(SONG_DIR)) {
+			try {
+				Files.createDirectories(SONG_DIR);
+			}
+				catch (IOException e) {}
+		}
+		if (!Files.exists(SONG_FILE)) {
+			try {
+				Files.createDirectories(SONG_FILE);
+			}
+				catch (IOException e) {}
+		}
+		if (!Files.exists(PLAYLIST_PATH)) {
+			try {
+				Files.createDirectories(PLAYLIST_PATH);
+			}
+				catch (IOException e) {}
+		}
 	}
+
+	
+	public static void addChatMessage(String message) {
+		MC.player.sendMessage(Text.of(message), false);
+	}
+
+	public static void addChatMessage(Text text) {
+		MC.player.sendMessage(text, false);
+	}
+	
 }
