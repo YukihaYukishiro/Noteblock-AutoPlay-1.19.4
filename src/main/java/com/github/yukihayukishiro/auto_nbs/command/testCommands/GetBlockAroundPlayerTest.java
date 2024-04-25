2f96919e804ee063fa18e832b3335d71fb6e1dde
YukihaYukishiro <appletea0514@gmail.com>
2024-04-25 18:19:11 +0900

    getting the block around the player test

--- src/main/java/com/github/yukihayukishiro/auto_nbs/command/testCommands/GetBlockAroundPlayerTest.java
+++ src/main/java/com/github/yukihayukishiro/auto_nbs/command/testCommands/GetBlockAroundPlayerTest.java
@@ -1,13 +1,16 @@
 package com.github.yukihayukishiro.auto_nbs.command.testCommands;
 
+import com.github.yukihayukishiro.auto_nbs.AutoNbsPlayer;
 import com.github.yukihayukishiro.auto_nbs.utils.GetBlocksInRange;
 import com.mojang.brigadier.context.CommandContext;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.Mutable;
 
 public class GetBlockAroundPlayerTest {
     public static int run(CommandContext<ServerCommandSource> context) {
@@ -17,7 +20,21 @@ public class GetBlockAroundPlayerTest {
         BlockPos playerPos = player.getBlockPos();
         player.sendMessage(Text.of("Player position: " + playerPos.toString()), false);
         Block[][][] area = GetBlocksInRange.getBlocksInRange(playerPos);
-        player.sendMessage(Text.of(, false);
+        // convert to string
+        String[][][] areaString = new String[area.length][area[0].length][area[0][0].length];
+        String temp;
+        for (int y = 0; y < area.length; y++) {
+            player.sendMessage(Text.of("=====[ " + y + " ]====="), false);
+            for (int x = 0; x < area[0].length; x++) {
+                temp = "x" + x + "=>";
+                for (int z = 0; z < area[0][0].length; z++) {
+                    areaString[x][y][z] = "[ " + area[x][y][z].getName().getString() + " ]";
+                    temp += areaString[x][y][z] + ",";
+                }
+                player.sendMessage(Text.of(temp), false);
+            }
+        }
+        player.sendMessage(Text.of("==============="), false);
         return 1;
     }
 }
--- src/main/java/com/github/yukihayukishiro/auto_nbs/utils/GetBlocksInRange.java
+++ src/main/java/com/github/yukihayukishiro/auto_nbs/utils/GetBlocksInRange.java
@@ -3,25 +3,30 @@ package com.github.yukihayukishiro.auto_nbs.utils;
 import com.github.yukihayukishiro.auto_nbs.AutoNbsPlayer;
 
 import net.minecraft.block.Block;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 
 public class GetBlocksInRange {
 
     public static Block[][][] getBlocksInRange(BlockPos pos) {
         Block[][][] blocks = new Block[5][5][5];
-        int x = pos.getX();
-        int y = pos.getY();
-        int z = pos.getZ();
+        int centerX = pos.getX();
+        int centerY = pos.getY();
+        int centerZ = pos.getZ();
         int range = 2;
-        for (int h = y - range; h <= y + range; h++) {
-            for (int i = x + range; i >= x - range; i--) {
-                for (int j = z + range; j >= z - range; j--) {
-                    BlockPos newPos = new BlockPos(i, 0, j);
+
+        for (int y = centerY - range; y <= centerY + range; y++) {
+            for (int x = centerX + range; x >= centerX - range; x--) {
+                for (int z = centerZ + range; z >= centerZ - range; z--) {
+
+                    BlockPos newPos = new BlockPos(x, y, z);
                     Block block = AutoNbsPlayer.MC.world.getBlockState(newPos).getBlock();
-                    blocks[i - x + range][h - y + range][j - z + range] = block;
+                    blocks[(centerX + range) - x][(centerY + range) - y][(centerZ + range) - z] = block;
                 }
             }
         }
+        AutoNbsPlayer.MC.player.sendMessage(Text.of("First = " + blocks[0][0][0].getName().getString()), false);
+        AutoNbsPlayer.MC.player.sendMessage(Text.of("Last = " + blocks[4][4][4].getName().getString()), false);
         return blocks;
 
     }
